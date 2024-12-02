2bb70a49c626db5e55e73583978eb8d97aa33f15
shivmSingh1 <shivams7380@gmai.com>
2024-12-03 05:05:28 +0530

    msg

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
-// vite.config.js
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-	// base: '/minorProject/', // Update with your repo name
-})
+	build: {
+		outDir: 'dist', // Ensure Vite's build output goes into the 'dist' folder
+		rollupOptions: {
+			input: {
+				main: 'index.html',  // Your landing page
+			},
+		},
+	},
+});
